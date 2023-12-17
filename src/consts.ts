5869cd8c788c95f502e20971bf4f98bb5b5ad7dd
H-ymt <y.handai1272@gmail.com>
2023-12-17 22:34:37 +0900

    refactor(sidemenu): メニューを定数化コンポーネント側で呼び出せるようにする

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,13 @@
 export const SITE_TITLE = "Component Asset";
 export const SITE_DESCRIPTION = "My UI Components assets.";
+
+export const menuComponents = [
+  {
+    label: "Hamburger Menu",
+    path: "/hamburger-menu",
+  },
+  {
+    label: "Accordion",
+    path: "/accordion",
+  },
+];
