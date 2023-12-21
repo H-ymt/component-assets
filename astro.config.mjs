cfff17b946bf75944f439f062cc18e611f88163d
H-ymt <y.handai1272@gmail.com>
2023-12-21 22:11:23 +0900

    feat(components): サイトに掲載するコンポーネントの実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from "astro/config";
-import expressiveCode from "astro-expressive-code";
-
 import mdx from "@astrojs/mdx";
+import expressiveCode from "astro-expressive-code";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [expressiveCode(), mdx()]
+  integrations: [expressiveCode(), mdx()],
 });
