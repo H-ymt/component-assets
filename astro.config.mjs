719b891fb48bd5a406aa5d2deeb0ef7082f7f680
H-ymt <y.handai1272@gmail.com>
2023-12-25 21:59:10 +0900

    build(package): ReactとFloating UIをインストールする

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import mdx from "@astrojs/mdx";
 import expressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [expressiveCode(), mdx()],
+  integrations: [expressiveCode(), mdx(), react()]
 });
