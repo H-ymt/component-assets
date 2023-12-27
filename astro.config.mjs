8095f22da6cbc5e8aab4defc56fac700bf971c8d
H-ymt <y.handai1272@gmail.com>
2023-12-27 13:32:44 +0900

    feat(menumobile): モバイルメニューの実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import mdx from "@astrojs/mdx";
 import expressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
